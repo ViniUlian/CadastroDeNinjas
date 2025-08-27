72df3e4533a45fd6b2a41c7cf7ae532aab0771c3
Vinicius Ulian <viniciusulian1206@gmail.com>
2025-08-27 17:11:24 -0300

    feat: Cria o Mapper e o DTO para a entidade missoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    private List<NinjaModel> ninjas;
+}
