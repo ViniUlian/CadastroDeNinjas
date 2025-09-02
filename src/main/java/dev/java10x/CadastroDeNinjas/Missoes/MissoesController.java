7558e95c95a7ff815589a63cfa390a7167d6eeca
Vinicius Ulian <viniciusulian1206@gmail.com>
2025-09-02 10:13:51 -0300

    refactor: Refatora o MissoesController para incluir o ResponseEntity nas minhas rotas e costumizar a resposta das requisicoes do cliente

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,7 +1,10 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Repeatable;
 import java.util.List;
 
 @RestController
@@ -16,31 +19,52 @@ public class MissoesController {
 
     // Mostrar Missão
     @GetMapping("/listar")
-    public List<MissoesDTO> listarMissao(){
-        return missoesService.listarMissao();
+    public ResponseEntity<List<MissoesDTO>> listarMissao(){
+        List<MissoesDTO> missoes = missoesService.listarMissao();
+        return ResponseEntity.ok(missoes);
     }
 
     // Mostrar Missão por Id
     @GetMapping("/listar/{id}")
-    public MissoesDTO listarMissaoPorId(@PathVariable Long id){
-        return missoesService.listarMissaoPorId(id);
+    public ResponseEntity<?> listarMissaoPorId(@PathVariable Long id){
+        if(missoesService.listarMissaoPorId(id) != null){
+            MissoesDTO missao = missoesService.listarMissaoPorId(id);
+            return ResponseEntity.ok(missao);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Missão não encontrada");
+        }
     }
 
     // Adicionar Missao
     @PostMapping("/criar")
-    public MissoesDTO criarMissao(@RequestBody MissoesDTO missao){
-        return missoesService.criarMissao(missao);
+    public ResponseEntity<String> criarMissao(@RequestBody MissoesDTO missao){
+        MissoesDTO novaMissao = missoesService.criarMissao(missao);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Missão criada com sucesso");
     }
 
     // Alterar dados da Missão
     @PutMapping("/alterar/{id}")
-    public MissoesDTO alterarMissao(@PathVariable Long id, @RequestBody MissoesDTO missaoAtualizada){
-        return missoesService.atualizaMissao(id, missaoAtualizada);
+    public ResponseEntity<?> alterarMissao(@PathVariable Long id, @RequestBody MissoesDTO missaoDTO){
+        if(missoesService.listarMissaoPorId(id) != null){
+            MissoesDTO missaoAtualizada = missoesService.atualizaMissao(id, missaoDTO);
+            return ResponseEntity.ok(missaoAtualizada);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Missão não encontrada");
+        }
     }
 
     // Deletar Missão
     @DeleteMapping("/deletar/{id}")
-    public void deletarMissaoPorId(@PathVariable Long id){
-        missoesService.deletarMissaoPorId(id);
+    public ResponseEntity<String> deletarMissaoPorId(@PathVariable Long id){
+        if (missoesService.listarMissaoPorId(id) != null){
+            missoesService.deletarMissaoPorId(id);
+            return ResponseEntity.ok("Missão deletada com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Missão não encontrada");
+        }
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,5 +1,8 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
 
     public MissoesModel map(MissoesDTO missoesDTO){
